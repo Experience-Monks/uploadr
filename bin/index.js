53679eab27b2038e6012969426f4b6a6f77fcb9e
mattdesl <dave.des@gmail.com>
2016-05-27 14:04:52 -0400

    better error for no env file

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
+var chalk = require('chalk');
 var argv = require('./parse-args')();
-
 var uploadr = require('../');
 var log = require('../lib/log');
 
 uploadr(argv).on('error', function (err) {
-  log.error(err.message);
-  console.error(err.stack.split('\n').slice(1).join('\n'));
+  if (err.type === 'NO_ENV_FILE') {
+    log.error('No ' + chalk.bold('.env') + ' file found in current directory\n' +
+      'A JSON file with ' + chalk.bold('{ username, password }') +
+      ' required for FTP authentication.');
+  } else {
+    log.error(err.message);
+    console.error(err.stack.split('\n').slice(1).join('\n'));
+  }
   process.exit(1);
 });
